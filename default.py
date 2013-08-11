be221a04a7b13ff6c68a594868a0b960c29d98e0
Alex Waite <Alexqw85@gmail.com>
2013-08-11 14:05:22 +0200

    Initial code from Chris

---
+++ default.py
@@ -0,0 +1,112 @@
+﻿import urllib
+import urllib2
+import re
+import os
+import json
+import cookielib
+import xbmcplugin
+import xbmcgui
+import xbmcvfs
+import xbmcaddon
+import StorageServer
+from traceback import format_exc
+from urlparse import urlparse, parse_qs
+from BeautifulSoup import BeautifulSoup
+
+addon = xbmcaddon.Addon(id='plugin.video.nfl.gamepass')
+addon_path = xbmc.translatePath(addon.getAddonInfo('path'))
+addon_profile = xbmc.translatePath(addon.getAddonInfo('profile'))
+cookie_file = os.path.join(addon_profile, 'cookie_file')
+cookie_jar = cookielib.LWPCookieJar(cookie_file)
+icon = os.path.join(addon_path, 'icon.png')
+fanart = os.path.join(addon_path, 'fanart.jpg')
+base_url = ''
+debug = addon.getSetting('debug')
+addon_version = addon.getAddonInfo('version')
+cache = StorageServer.StorageServer("nfl_gamepass", 24)
+username = addon.getSetting('email')
+password = addon.getSetting('password')
+
+
+def addon_log(string):
+    if debug == 'true':
+        xbmc.log("[addon.nfl.gamepass-%s]: %s" %(addon_version, string))
+
+
+def make_request(url, data=None, headers=None):
+    addon_log('Request URL: %s' %url)
+    if headers is None:
+        headers = {'User-agent' : 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:22.0) Gecko/20100101 Firefox/22.0',
+                   'Referer' : base_url}
+    if not xbmcvfs.exists(cookie_file):
+        addon_log('Creating cookie_file!')
+        cookie_jar.save()
+    cookie_jar.load(cookie_file, ignore_discard=True, ignore_expires=True)
+    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookie_jar))
+    urllib2.install_opener(opener)
+    try:
+        req = urllib2.Request(url, data, headers)
+        response = urllib2.urlopen(req)
+        cookie_jar.save(cookie_file, ignore_discard=True, ignore_expires=False)
+        data = response.read()
+        addon_log(str(response.info()))
+        redirect_url = response.geturl()
+        response.close()
+        if redirect_url != url:
+                addon_log('Redirect URL: %s' %redirect_url)
+        return data
+    except urllib2.URLError, e:
+        addon_log('We failed to open "%s".' %url)
+        if hasattr(e, 'reason'):
+            addon_log('We failed to reach a server.')
+            addon_log('Reason: ', e.reason)
+        if hasattr(e, 'code'):
+            addon_log('We failed with error code - %s.' %e.code)
+
+
+def gamepass_login():
+    url = 'https://gamepass.nfl.com/nflgp/secure/login?redirect=loginform&redirectnosub=packages&redirectsub=schedule'
+    post_data = {'username': username, 'vendor_id': password}
+    login_data = make_request(url, urllib.urlencode(post_data))
+    addon_log('login responce: %s' %login_data)
+
+
+def add_dir(name, url, mode, iconimage):
+    params = {'name': name, 'url': url, 'mode': mode}
+    url = '%s?%s' %(sys.argv[0], urllib.urlencode(params))
+    listitem = xbmcgui.ListItem(name, iconImage=iconimage, thumbnailImage=iconimage)
+    listitem.setProperty("Fanart_Image", fanart)
+    xbmcplugin.addDirectoryItem(int(sys.argv[1]), url, listitem, True)
+
+
+def get_params():
+    p = parse_qs(sys.argv[2][1:])
+    for i in p.keys():
+        p[i] = p[i][0]
+    return p
+
+
+if debug == 'true':
+    cache.dbg = True
+params = get_params()
+addon_log("params: %s" %params)
+
+try:
+    mode = int(params['mode'])
+except:
+    mode = None
+
+if mode == None:
+    add_dir('Login', 'login', 1, icon)
+    xbmcplugin.endOfDirectory(int(sys.argv[1]))
+
+elif mode == 1:
+    gamepass_login()
+    xbmcplugin.endOfDirectory(int(sys.argv[1]))
+
+elif mode == 2:
+    # add_plugin dir
+    xbmcplugin.endOfDirectory(int(sys.argv[1]))
+
+elif mode == 3:
+    resolve_url(params['url'])
