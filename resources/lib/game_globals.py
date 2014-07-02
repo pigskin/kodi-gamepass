8b0321b07d9e6d1112afebc68d275a59efb2b200
BaumSchorle <raffael.gisinger@gmail.com>
2014-07-02 17:23:37 +0200

    Bugfix of fix ;)

--- resources/lib/game_globals.py
+++ resources/lib/game_globals.py
@@ -34,7 +34,7 @@ if subscription == '0': # game pass
         'A Football Life': {'2014': '218', '2013': '186', '2012': '154'},
         'Superbowl Archives': {'2014': '117'},
         'NFL Films Presents': {'2014': '219', '2013': '187'},
-        'Hard Knocks': {'2013': '220'}
+        'Hard Knocks': {'2014': '220'}
         }
         
 else: # game rewind
