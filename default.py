c8df0edb54f72b7fdc470997eb10859e064ad336
Patrick Maguire <pmaguire@twitter.com>
2015-09-04 20:32:00 -0400

    Fixing bug to set the proxy scheme to one of http|https

--- default.py
+++ default.py
@@ -43,7 +43,7 @@ else:
 proxy_config = None
 if addon.getSetting('proxy_enabled') == 'true':
     proxy_config = {
-        'scheme': addon.getSetting('proxy_scheme'),
+        'scheme': 'https' if addon.getSetting('proxy_scheme') == '1' else 'http',
         'host': addon.getSetting('proxy_host'),
         'port': addon.getSetting('proxy_port'),
         'auth': {
