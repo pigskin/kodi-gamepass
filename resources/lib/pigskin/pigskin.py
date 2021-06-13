20b96724dd5ded1525477aca531b2dd8bb3ab13f
Thomas Hager <duke@sigsegv.at>
2021-06-13 11:16:28 +0200

    Introduce fix for #430 and clean-up the code in _get_diva_streams.
    We have a make_request() method, so we should use it.

--- resources/lib/pigskin/pigskin.py
+++ resources/lib/pigskin/pigskin.py
@@ -931,6 +931,14 @@ class pigskin(object):
             'Connection': 'keep-alive',
             'User-Agent': self.user_agent
         }
+
+        # Diva added some more validation to their API. Any content type besides
+        # application/json is refused
+        diva_header = {
+            'Connection': 'keep-alive',
+            'User-Agent': self.user_agent,
+            'Content-Type': 'application/json'
+        }
         for vs in akamai_xml.iter('videoSource'):
             try:
                 vs_format = vs.attrib['name'].lower()
@@ -941,12 +949,7 @@ class pigskin(object):
             payload = self._build_processing_url_payload(video_id, vs_url)
 
             try:
-                r = self.http_session.post(url=processing_url, data=payload)
-                self._log_request(r)
-                data = r.json()
-            except ValueError:
-                self.logger.error('_get_diva_streams: server response is invalid')
-                continue
+                data = self.make_request(url=processing_url,method='post',payload=payload,headers=diva_header)
             except Exception as e:
                 raise e
 
