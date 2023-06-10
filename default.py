5c5adf17b6a186e2ecbe27e0618589481661cdfe
AleXSR700 <57034871+AleXSR700@users.noreply.github.com>
2023-06-10 23:14:20 +0200

    Update default.py

--- default.py
+++ default.py
@@ -288,7 +288,7 @@ class GamepassGUI(xbmcgui.WindowXML):
                         str(timedelta(seconds=int(float(game['video']['videoDuration'].replace(',', '.'))))))
                 else:
                     game_info = game['phase'] + '\n' + '(' + datetime_obj.strftime(datetime_format) +')'
-                    if addon.getSetting('hide_game_length') == 'true' and game_info == 'FINAL_OVERTIME':
+                    if addon.getSetting('hide_game_length') == 'true' and game_info == game['phase'] + '\n' + '(' + datetime_obj.strftime(datetime_format) +')':
                         game_info = 'FINAL (%s)' % (datetime_obj.strftime(datetime_format))
             else:
                 game_info = datetime_obj.strftime(datetime_format)
